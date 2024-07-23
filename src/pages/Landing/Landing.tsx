adc626b224ee8303ba6729251560f9cf7e7e7d4e
A-mont <a.mont.24.777@gmail.com>
2024-07-22 19:53:03 -0600

    Add Multiwallet Component

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,13 +1,22 @@
-import { Center } from "@chakra-ui/react";
+import { Heading } from "@/components/ui/heading";
+import { Wallet } from "@/features/multiwallet";
+import { MultiWallet } from "@/features/multiwallet/ui/wallet";
+import { Center, VStack } from "@chakra-ui/react";
 import { Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function Landing() {
   return (
     <Center>
+      <VStack>
       <Button textColor="black" bg="#00ffc4" as={Link} to="/home">
         Examples
       </Button>
+      <VStack>
+      <Heading size="xs">Multiwallet Component</Heading>
+      <MultiWallet/>
+      </VStack>
+      </VStack>
       
     </Center>
   );
