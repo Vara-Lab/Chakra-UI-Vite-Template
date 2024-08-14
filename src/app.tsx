120ff338f78643d3b56085e99386f7d16c3ec022
David-HernandezM <dh29550@gmail.com>
2024-08-14 01:32:59 -0600

    Update app.tsx
    Deleted line, it is necessary to use vara-ui

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import { Header } from "@/components/layout";
 import { withProviders } from "@/app/hocs";
 import { useWalletSync } from "@/features/wallet/hooks";
 import { Routing } from "./pages";
+import "@gear-js/vara-ui/dist/style.css";
 
 function Component() {
   const { isApiReady } = useApi();
